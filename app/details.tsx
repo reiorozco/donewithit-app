8c9774cfbeb8f00e7f85beea2bb134445e17d130
reiorozco <rfoc15@gmail.com>
2023-08-21 17:39:37 -0500

    Navigation Navigate between pages Pass Parameters to routes

---
+++ app/details.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, Text } from "react-native";
+import { Stack } from "expo-router/stack";
+import { useLocalSearchParams, useRouter } from "expo-router";
+
+import Screen from "./components/Screen";
+
+function TweetDetails() {
+  const router = useRouter();
+  const { name } = useLocalSearchParams();
+
+  return (
+    <Screen>
+      <Stack.Screen
+        options={{
+          title: name as string,
+
+          headerStyle: { backgroundColor: "#f4511e" },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+
+      <Text>Tweets Details</Text>
+
+      <Button
+        title="Fix title"
+        onPress={() => router.setParams({ name: "Tweet Details" })}
+      />
+    </Screen>
+  );
+}
+
+export default TweetDetails;
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,32 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { Button, StyleSheet, Text } from "react-native";
+import { Stack } from "expo-router/stack";
+import { useRouter } from "expo-router";
+
+import Screen from "./components/Screen";
 
 import colors from "./config/colors";
 
 export default function App() {
+  const router = useRouter();
+
   return (
-    <SafeAreaView>
-      <Text>Home Screen</Text>
-    </SafeAreaView>
+    <Screen>
+      <Stack.Screen options={{ title: "Tweets" }} />
+
+      <Text>Tweets</Text>
+
+      <Button
+        title="View Tweet"
+        onPress={() => {
+          router.push({
+            pathname: "details",
+            params: { name: "Press fix title" },
+          });
+        }}
+      />
+
+      {/*<Link href="/details">View tweet</Link>*/}
+    </Screen>
   );
 }
 
