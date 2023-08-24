548e52a3cebc38eb9dd48d74efac8acadee73e70
reiorozco <rfoc15@gmail.com>
2023-08-24 08:36:38 -0500

    Navigation Beautify the tabs

--- app/(tabs)/(feed)/_layout.tsx
+++ app/(tabs)/(feed)/_layout.tsx
@@ -2,16 +2,10 @@ import { Stack } from "expo-router/stack";
 
 export default function Layout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ title: "Listings" }} />
 
-      <Stack.Screen
-        name="listingDetails"
-        options={{
-          headerShown: false,
-          presentation: "modal",
-        }}
-      />
+      <Stack.Screen name="listingDetails" options={{ presentation: "modal" }} />
     </Stack>
   );
 }
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,20 @@
-import { Tabs } from "expo-router";
+import { Tabs, useRouter } from "expo-router";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+
+import colors from "../../src/config/colors";
 
 export default function Layout() {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveBackgroundColor: "tomato",
-        tabBarActiveTintColor: "white",
-        tabBarInactiveBackgroundColor: "#eee",
-        tabBarInactiveTintColor: "black",
-      }}
+      screenOptions={
+        {
+          // tabBarActiveBackgroundColor: "tomato",
+          // tabBarActiveTintColor: "white",
+          // tabBarInactiveBackgroundColor: "#eee",
+          // tabBarInactiveTintColor: "black",
+        }
+      }
     >
       <Tabs.Screen
         name="(feed)"
@@ -29,9 +34,11 @@ export default function Layout() {
           title: "Listing Edit",
           headerShown: false,
 
+          tabBarButton: () => <NewListingButton />,
+
           tabBarIcon: ({ size, color }) => (
             <MaterialCommunityIcons
-              name="application-edit"
+              name="plus-circle"
               size={size}
               color={color}
             />
@@ -53,3 +60,33 @@ export default function Layout() {
     </Tabs>
   );
 }
+
+function NewListingButton() {
+  const router = useRouter();
+
+  return (
+    <TouchableOpacity onPress={() => router.push("listingEdit")}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name="plus-circle"
+          size={40}
+          color={colors.white}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.primary,
+    borderColor: colors.white,
+    borderRadius: 40,
+    borderWidth: 5,
+    bottom: 15,
+    height: 80,
+    justifyContent: "center",
+    width: 80,
+  },
+});
