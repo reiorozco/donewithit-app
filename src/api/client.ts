43f6f5e117c1cae7e97df32cfc197bfdfaed18df
reiorozco <rfoc15@gmail.com>
2023-09-11 16:24:55 -0500

    Offline Support Cache successful request data

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,29 @@
-import { create } from "apisauce";
+import { create, ApiResponse } from "apisauce";
+import { AxiosRequestConfig } from "axios";
+import cache from "../utility/cache";
 
 // define the api
 const apiClient = create({
   baseURL: "http://192.168.1.4:9000/api",
 });
+
+const get = apiClient.get;
+
+apiClient.get = async <T, U = T>(
+  url: string,
+  params?: {},
+  axiosConfig?: AxiosRequestConfig
+): Promise<ApiResponse<T, U>> => {
+  const response = await get(url, params, axiosConfig);
+
+  if (response.ok) {
+    await cache.store(url, response.data);
+
+    return response;
+  }
+
+  const data = await cache.getStoreData(url);
+  return data ? { ok: true, data } : response;
+};
 
 export default apiClient;
