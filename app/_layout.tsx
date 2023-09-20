e07a19217c8fe85bb384f8e26982402a81abef97
reiorozco <rfoc15@gmail.com>
2023-09-20 07:56:38 -0500

    Offline Support Show an Offline Notice

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router/stack";
 import { ThemeProvider } from "@react-navigation/native";
 
+import OfflineNotice from "../src/components/OfflineNotice";
 import navigationTheme from "../src/config/navigationTheme";
 
 export default function Layout() {
   return (
     <ThemeProvider value={navigationTheme}>
+      <OfflineNotice />
+
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
 
