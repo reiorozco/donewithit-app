3c70e96634801f6d2e59ab63f5ef256a85fb1951
reiorozco <rfoc15@gmail.com>
2023-09-20 09:12:20 -0500

    Authentication and Authorization Authentication API

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,17 +1,28 @@
+/**
+ * Custom API client that extends apisauce's default behavior.
+ */
 import { create, ApiResponse } from "apisauce";
 import { AxiosRequestConfig } from "axios";
 import cache from "../utility/cache";
 
-// define the api
+// Define the api
 const apiClient = create({
   baseURL: "http://192.168.1.4:9000/api",
 });
 
 const get = apiClient.get;
 
+/**
+ * Custom get method that handles caching of responses.
+ *
+ * @param url - The API endpoint URL.
+ * @param params - Optional request parameters.
+ * @param axiosConfig - Optional Axios request configuration.
+ * @returns A promise that resolves to an ApiResponse.
+ */
 apiClient.get = async <T, U = T>(
   url: string,
-  params?: {},
+  params?: Record<string, any>,
   axiosConfig?: AxiosRequestConfig
 ): Promise<ApiResponse<T, U>> => {
   const response = await get(url, params, axiosConfig);
@@ -19,11 +30,13 @@ apiClient.get = async <T, U = T>(
   if (response.ok) {
     await cache.store(url, response.data);
 
-    return response;
+    return response as ApiResponse<T, U>;
   }
 
   const data = await cache.getStoreData(url);
-  return data ? { ok: true, data } : response;
+  return data
+    ? ({ ok: true, data } as ApiResponse<T, U>)
+    : (response as ApiResponse<T, U>);
 };
 
 export default apiClient;
