cc2ded807528f4e8d361e958629ee2002480c3a5
reiorozco <rfoc15@gmail.com>
2025-05-02 17:50:26 -0500

    Add WelcomeScreen component

---
+++ screens/WelcomeScreen.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Image, ImageBackground } from "expo-image";
+import { StyleSheet, Text, View } from "react-native";
+
+const backgroundSource = require("@/assets/images/background.jpg");
+const logoSource = require("@/assets/images/logo.png");
+
+function WelcomeScreen() {
+  return (
+    <ImageBackground
+      blurRadius={5}
+      source={backgroundSource}
+      style={styles.background}
+    >
+      <View style={styles.logoContainer}>
+        <Image source={logoSource} style={styles.logo} />
+
+        <Text style={styles.tagLine}>Sell What You Don&#39;t Need</Text>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  logo: {
+    height: 100,
+    width: 100,
+  },
+  logoContainer: {
+    alignItems: "center",
+    position: "absolute",
+    top: 70,
+  },
+  tagLine: {
+    fontSize: 24,
+    fontWeight: "500",
+    paddingVertical: 20,
+  },
+});
+
+export default WelcomeScreen;
