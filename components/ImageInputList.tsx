bf3f8bbcfe59a49947920f3026f30d150eab7f35
reiorozco <rfoc15@gmail.com>
2025-05-04 16:21:19 -0500

    Add ImageInputList component with scrollable image management

---
+++ components/ImageInputList.tsx
@@ -0,0 +1,48 @@
+import React, { useRef } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+
+import ImageInput from "@/components/ImageInput";
+
+interface Props {
+  images: (string | null)[];
+  onAddImage: (image: string | null) => void;
+  onRemoveImage: (image: string | null) => void;
+}
+
+function ImageInputList({ images, onAddImage, onRemoveImage }: Props) {
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  return (
+    <View>
+      <ScrollView
+        horizontal
+        onContentSizeChange={() =>
+          scrollViewRef.current?.scrollToEnd({ animated: true })
+        }
+        ref={scrollViewRef}
+      >
+        <View style={styles.container}>
+          {images.map((image) => (
+            <ImageInput
+              image={image}
+              key={image}
+              setImage={() => onRemoveImage(image)}
+            />
+          ))}
+          <ImageInput setImage={onAddImage} />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    columnGap: 10,
+    flexDirection: "row",
+    padding: 10,
+    width: "100%",
+  },
+});
+
+export default ImageInputList;
