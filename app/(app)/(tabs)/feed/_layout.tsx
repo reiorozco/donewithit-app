3da32ce6bbcd65b925197d18532a81a9a23e0dba
reiorozco <rfoc15@gmail.com>
2025-05-14 21:35:51 -0500

    Add push notification setup using expo-notifications
    Integrated `expo-notifications` to enable push notifications. Updated `app.json` with required permissions and added related setup in `_layout.tsx`, including token registration and notification handlers.

--- app/(app)/(tabs)/feed/_layout.tsx
+++ app/(app)/(tabs)/feed/_layout.tsx
@@ -1,10 +1,66 @@
 import { Stack } from "expo-router";
+import { useEffect, useRef, useState } from "react";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+import { Platform } from "react-native";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 export const unstable_settings = {
   initialRouteName: "index",
 };
 
 export default function FeedLayout() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  // const [channels, setChannels] = useState<Notifications.NotificationChannel[]>(
+  //   [],
+  // );
+  // const [notification, setNotification] = useState<
+  //   Notifications.Notification | undefined
+  // >(undefined);
+  // const notificationListener = useRef<Notifications.EventSubscription>(null);
+  // const responseListener = useRef<Notifications.EventSubscription>(null);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(
+      (token) => token && setExpoPushToken(token),
+    );
+
+    // if (Platform.OS === "android") {
+    //   Notifications.getNotificationChannelsAsync().then((value) =>
+    //     setChannels(value ?? []),
+    //   );
+    // }
+    // notificationListener.current =
+    //   Notifications.addNotificationReceivedListener((notification) => {
+    //     setNotification(notification);
+    //   });
+    //
+    // responseListener.current =
+    //   Notifications.addNotificationResponseReceivedListener((response) => {
+    //     console.log(response);
+    //   });
+    //
+    // return () => {
+    //   notificationListener.current &&
+    //     Notifications.removeNotificationSubscription(
+    //       notificationListener.current,
+    //     );
+    //   responseListener.current &&
+    //     Notifications.removeNotificationSubscription(responseListener.current);
+    // };
+  }, []);
+
+  console.log("expoPushToken: ", expoPushToken);
+
   return (
     <Stack
       screenOptions={{
@@ -19,4 +75,54 @@ export default function FeedLayout() {
       />
     </Stack>
   );
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("myNotificationChannel", {
+      importance: Notifications.AndroidImportance.MAX,
+      lightColor: "#FF231F7C",
+      name: "A channel is needed for the permissions prompt to appear",
+      vibrationPattern: [0, 250, 250, 250],
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get a push token for push notification!");
+      return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    // EAS projectId is used here.
+    try {
+      const projectId =
+        Constants?.expoConfig?.extra?.eas?.projectId ??
+        Constants?.easConfig?.projectId;
+      if (!projectId) {
+        throw new Error("Project ID not found");
+      }
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(token);
+    } catch (e) {
+      token = `${e}`;
+    }
+  } else {
+    alert("Must use a physical device for Push Notifications");
+  }
+
+  return token;
 }
